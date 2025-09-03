ea6ca1553e2c2d8f2da9da91dc3dd681bdd2e960
brunogsiq <brunogsiq@gmail.com>
2025-09-03 17:58:07 -0300

    configuracao

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+module.exports = defineConfig(
+{
+    viewportWidth: 1300,
+    viewportHeight: 800,
+    video: true,
+    chromeWebSecurity: false,
+    e2e: {
+        defaultCommandTimeout: 9000,
+        experimentalRunAllSpecs: true,
+        hideXHRInCommandLog: true,
+        baseUrl: "https://www.automationexercise.com/",
+        //Aqui ficarão todos os meus testes:
+        setupNodeEvents(on, config) {
+          // implement node event listeners here
+        },
     },
-  },
 });
